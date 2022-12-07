8b389f067e5fec920413b30699f0305875d06619
AJINKYA <radheshyamsd21@gmail.com>
2022-12-07 11:44:58 +0530

    prime problem solved

---
+++ prime.js
@@ -0,0 +1,23 @@
+Problem 1 : Check whether a number is Prime or not.
+
+
+
+let num = 17;
+
+let i=2;
+
+let count=0;
+
+for (let i=2; i<=num; i++){
+
+if(i%num)
+
+count++
+}
+
+if(factor==1){
+ console.log("It is a prime")
+}
+else{
+console.log("It is not a prime")
+}
